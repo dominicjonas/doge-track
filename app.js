cf24cae6d3162f4f2ea263c1c9a62d12ec1c8402
Dominic <ppkk.dominic@gmail.com>
2021-05-29 19:25:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let ws = new WebSocket(`wss://stream.binance.com:9443/ws/dogegbp@trade`);
+let stockPriceEl = document.querySelector("#display");
+let lastPrice = null;
+
+ws.onmessage = (e) => {
+  let stockObj = JSON.parse(e.data);
+
+  console.log(stockObj.p);
+
+  let price = parseFloat(stockObj.p).toFixed(4);
+  stockPriceEl.innerHTML = price;
+
+  stockPriceEl.style.color =
+    !lastPrice || lastPrice === price
+      ? "black"
+      : price > lastPrice
+      ? "limegreen"
+      : "red";
+
+  lastPrice = price;
+};
+
+if ((stockPriceEl.style.color = "limegreen")) {
+  console.log("up");
+} else if ((stockPriceEl.style.color = "red")) {
+  console.log("down");
+}
